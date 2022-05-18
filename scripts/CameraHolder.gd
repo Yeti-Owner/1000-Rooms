8d67c2f2f02545bff45ac8428cb66b7e04e9cd7a
1Ross78 <calrossmaclean@gmail.com>
2022-05-18 17:52:00 -0400

    Basic World and Player created
    Added Player + Camera with full code. Also created basic world with floor and 2 walls.

---
+++ scripts/CameraHolder.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+var mouse_sensitivity := 0.18
+
+onready var _camera := $Camera
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion && Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		_camera.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		_camera.rotation_degrees.x = clamp(_camera.rotation_degrees.x, -89.9, 89.9)
---
+++ scripts/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody
+var walk_speed := 6.0
+var jump_speed := 8.0
+var acceleration_speed := 6.5
+var gravity := -14.0
+var _dir := Vector3.ZERO
+var _vel := Vector3.ZERO
+onready var _camera := $CameraHolder
+
+
+func _physics_process(delta: float) -> void:
+	var input := Vector2.ZERO
+	
+	if Input.is_action_pressed("up"):
+		input.y += 1
+	if Input.is_action_pressed("down"):
+		input.y -= 1
+	if Input.is_action_pressed("right"):
+		input.x += 1
+	if Input.is_action_pressed("left"):
+		input.x -= 1
+	
+	input = input.normalized()
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			_vel.y = jump_speed
+	
+	_vel.y += gravity * delta
+	
+	var basis: Basis = _camera.global_transform.basis
+	_dir = Vector3.ZERO
+	_dir += -basis.z * input.y
+	_dir += basis.x * input.x
+	_dir = _dir.normalized()
+	
+	var acc := _vel.linear_interpolate(_dir * walk_speed, acceleration_speed * delta)
+	_vel = move_and_slide(Vector3(acc.x, _vel.y, acc.z), Vector3.UP)
+
+
+
+
+
+
