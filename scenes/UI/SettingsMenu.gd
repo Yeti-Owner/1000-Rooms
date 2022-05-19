11d08374410fd54cfecedd33032d34eb28ab3b3d
1Ross78 <calrossmaclean@gmail.com>
2022-05-19 10:53:45 -0400

    Created options menu
    the layout for it is created but functionality is not set up yet

---
+++ scenes/UI/SettingsMenu.gd
@@ -0,0 +1,9 @@
+extends Popup
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_DisplayOptionBtn_item_selected(index):
+	pass # Replace with function body.
--- scripts/StartMenu.gd
+++ scripts/StartMenu.gd
@@ -5,7 +5,7 @@ func _on_StartBtn_pressed():
 	get_tree().change_scene("res://scenes/world.tscn")
 
 func _on_OptionsBtn_pressed():
-	pass # Replace with function body.
+	$SettingsMenu.popup_centered()
 
 func _on_QuitBtn_pressed():
 	get_tree().quit()
