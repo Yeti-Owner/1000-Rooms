57fda87ef335118dd5a43f3ad73c6a1c96d7f508
1Ross78 <calrossmaclean@gmail.com>
2022-05-19 13:25:02 -0400

    Added functionality to "Video" tab (mostly)

--- scenes/UI/SettingsMenu.gd
+++ scenes/UI/SettingsMenu.gd
@@ -1,9 +1,34 @@
 extends Popup
+var VsyncEnabled = 0
+
+# Below is Video tab
+# code not made: ShowFps, Bloom, Brightness
+
+# Windowed/Fullscreen option
+func _on_DisplayOptionBtn_item_selected(FullScreenIndex):
+	match FullScreenIndex:
+		0:
+			OS.set_window_fullscreen(false)
+		1:
+			OS.set_window_fullscreen(true)
+
+# Vsync
+func _on_VsyncCheckBtn_pressed():
+	VsyncEnabled = !VsyncEnabled
+	OS.set_use_vsync(VsyncEnabled)
+	print(VsyncEnabled)
+
+# Max Fps
+func _on_MaxFpsSlider_value_changed(MaxFps):
+	$SettingsTabs/Video/MarginContainer/VideoSettings/HBoxContainer/MaxFpsValue.set_text(str(MaxFps))
+	Engine.set_target_fps(int(MaxFps))
 
 
-func _ready():
-	pass # Replace with function body.
+# Below is Audio Tab
+# Nothing is added lmao
+
+# placeholder
 
 
-func _on_DisplayOptionBtn_item_selected(index):
-	pass # Replace with function body.
+# Below is Gameplay Tab
+# Nothing is added here either lmao
