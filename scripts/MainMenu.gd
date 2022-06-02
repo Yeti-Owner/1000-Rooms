6bf2b10f061914b2bcb2bd22f0054843a978467e
1Ross78 <calrossmaclean@gmail.com>
2022-06-01 22:16:32 -0400

    MASSIVE improvement to start menu

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+var dir = Directory.new()
+var save_game = "user://save_game.dat"
+onready var fader = get_parent()
+onready var ClickPlayer = get_node("SettingsMenu/ClickPlayer")
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	fader.connect("fade_finished", self, "on_fade_finished")
+
+#func _process(delta):
+#	pass
+
+func _on_StartBtn_pressed():
+	ClickPlayer._click_sound()
+	fader._fade_out()
+
+func _on_OptionsBtn_pressed():
+	ClickPlayer._click_sound()
+	$SettingsMenu.popup_centered()
+
+func _on_ClearSaveBtn_pressed():
+	ClickPlayer._click_sound()
+	dir.remove(save_game)
+	get_tree().quit()
+
+func _on_QuitBtn_pressed():
+	ClickPlayer._click_sound()
+	Settingsholder._save()
+	get_tree().quit()
+
+func on_fade_finished():
+	var _error = get_tree().change_scene(Settingsholder.CurrentRoom)
---
+++ scripts/StartMenuCamera.gd
@@ -0,0 +1,54 @@
+extends Camera
+
+var RNG
+var Last
+#var pos1 = Vector3(-23.686, 2.086, 18.111)
+#var pos2 = Vector3(-5.18, 2.086, 11.7)
+#var pos3 = Vector3(17.028, 0.775, 11.7)
+#var pos4 = Vector3(53.209, 1.569, 29.826)
+onready var Anim = $AnimationPlayer
+#onready var fader = get_parent()
+onready var timer = get_parent().get_node("Timer")
+
+
+func _ready():
+	randomize()
+	_change_cam()
+	timer.start()
+#	fader.connect("fade_finished", self, "on_fade_finished")
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	_change_cam()
+
+func _change_cam():
+	RNG = randi() % 4
+	if RNG == Last:
+		_change_cam()
+	elif RNG == 0:
+#		timer.start()
+#		self.set_translation(pos1)
+		Anim.play("loc1")
+		Last = RNG
+	elif RNG == 1:
+#		timer.start()
+#		self.set_translation(pos2)
+		Anim.play("loc2")
+		Last = RNG
+	elif RNG == 2:
+#		timer.start()
+#		self.set_translation(pos3)
+		Anim.play("loc3")
+		Last = RNG
+	elif RNG == 3:
+#		timer.start()
+#		self.set_translation(pos4)
+		Anim.play("loc4")
+		Last = RNG
+
+func _on_Timer_timeout():
+#	fader._fade_out()
+	timer.start()
+
+#func on_fade_finished():
+#	fader._fade_in()
+#	timer.start()
