7f6603c1a81190ac72430c79af966882c0e11a97
1Ross78 <calrossmaclean@gmail.com>
2022-06-09 12:27:21 -0400

    Narrator + smaller assets
    Reimported assets for smaller export filesize. Added Narrator.

---
+++ scripts/Narrator.gd
@@ -0,0 +1,47 @@
+extends Control
+
+var messages = [""]
+
+var typing_speed = 0.07
+var read_time = 1.5
+
+var current_message = 0
+var display = ""
+var current_char = 0
+
+func _ready():
+	start_dialogue()
+
+func start_dialogue():
+	current_message = 0
+	display = ''
+	current_char = 0
+	
+	$next_char.set_wait_time(typing_speed)
+	$next_char.start()
+
+func stop_dialogue():
+	$dialogue.text = ""
+
+func _on_next_char_timeout():
+	if (current_char < len(messages[current_message])):
+		var next_char = messages[current_message][current_char]
+		display += next_char
+		
+		$dialogue.text = display
+		current_char += 1
+	else:
+		$next_char.stop()
+		$next_message.one_shot = true
+		$next_message.set_wait_time(read_time)
+		$next_message.start()
+
+
+func _on_next_message_timeout():
+	if (current_message == len(messages) - 1):
+		stop_dialogue()
+	else:
+		current_message += 1
+		display = ""
+		current_char = 0
+		$next_char.start()
--- scripts/load_world.gd
+++ scripts/load_world.gd
@@ -1,8 +1,14 @@
 extends Spatial
 
 onready var fader = $Fader
+onready var Narrator = $Narrator
 export(String) var _room 
 
 func _ready():
 	Settingsholder.CurrentRoom = _room
 	fader._fade_in()
+	_check_room()
+
+func _check_room():
+	if _room == "res://scenes/world.tscn":
+		Narrator.messages = ["Welcome to my dungeon", "have fun"]
