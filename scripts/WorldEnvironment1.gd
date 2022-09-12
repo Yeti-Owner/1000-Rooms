22be62fa0624e8345ae592dad9f0ecadc043c493
Yeti-Owner <calrossmaclean@gmail.com>
2022-09-12 17:57:20 -0400

    Changes to Settings + collisions

--- scripts/WorldEnvironment1.gd
+++ scripts/WorldEnvironment1.gd
@@ -7,13 +7,18 @@ func _ready():
 	Settingsholder.connect("bloom_changed", self, "_bloom")
 # warning-ignore:return_value_discarded
 	Settingsholder.connect("brightness_changed", self, "_brightness")
-
+	
+	_bloom()
 
 func _bloom():
 	if (Settingsholder.BloomSet):
 		environment.set_glow_bloom(0.75)
+		environment.glow_bicubic_upscale = true
+		environment.glow_high_quality = true
 	else:
 		environment.set_glow_bloom(0)
+		environment.glow_bicubic_upscale = false
+		environment.glow_high_quality = false
 
 func _brightness():
 	UsableBrightness = float(Settingsholder.Brightness)/10
